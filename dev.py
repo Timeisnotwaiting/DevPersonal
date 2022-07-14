e4d8de2143abde5ff4e476245cb63c1667744202
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-07-14 16:43:16 +0530

    Update dev.py

--- dev.py
+++ dev.py
@@ -4,9 +4,9 @@ import config
 Dev = Client(s_n = config.STRING, a_i = config.API_ID, a_h = config.API_HASH, plugins=dict(root="Deadbody"))
 
 if config.P != "R_a_Y":
-    return print("wrong password Bruhh !")
-
-Dev.start()
-Dev.idle()
-print("done vro !, made in pain")
-print("I'm Alive !")
+    print("wrong password Bruhh !")
+else:
+    Dev.start()
+    Dev.idle()
+    print("done vro !, made in pain")
+    print("I'm Alive !")
