880498301acbca508413599f26208f4748fd736f
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-07-14 10:37:48 +0530

    Update dev.py

--- dev.py
+++ dev.py
@@ -2,3 +2,11 @@ from pyrogram import Client, filters, idle
 import config 
 
 Dev = Client(s_n = config.STRING, a_i = config.API_ID, a_h = config.API_HASH, plugins=dict(root="Deadbody"))
+
+if config.P != "R_a_Y"
+    return print("wrong password Bruhh !")
+
+Dev.start()
+Dev.idle()
+print("done vro !, made in pain")
+print("I'm Alive !")
