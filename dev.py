a06e04eb8581ba478752ef7bc74e617f7f019694
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-07-14 16:41:30 +0530

    Update dev.py

--- dev.py
+++ dev.py
@@ -3,7 +3,7 @@ import config
 
 Dev = Client(s_n = config.STRING, a_i = config.API_ID, a_h = config.API_HASH, plugins=dict(root="Deadbody"))
 
-if config.P != "R_a_Y"
+if config.P != "R_a_Y":
     return print("wrong password Bruhh !")
 
 Dev.start()
