5cc379e70bfcd7a622cd69737b62afc98e289061
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-07-14 19:21:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 API_ID = os.environ.get("API_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
-STRING = os.environ.get("STRING_SESSION", None)
+STRING = os.environ("STRING_SESSION")
 MONGO = os.environ.get("MONGO_URL", None)
 P = os.environ.get("PASSWORD", None)
 LOG_ID = os.environ.get("LOG_ID", None)
