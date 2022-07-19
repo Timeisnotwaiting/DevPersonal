c6b3b5cf26667d9371ff175d446def7f90871d3f
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-07-19 18:46:48 +0530

    Update dev.py

--- dev.py
+++ dev.py
@@ -1,9 +1,13 @@
 import logging
 
 from telethon import events
+from telethon.sessions import StringSession
 from telethon import TelegramClient, Button
 from config import *
 
 omfoo = "https://te.legra.ph/file/9a93122c668f2e8dfb54f.jpg"
 
 logging.basicConfig(level=logging.INFO)
+
+DEV = TelegramClient(StringSession(STRING), API_ID, API_HASH)
+
