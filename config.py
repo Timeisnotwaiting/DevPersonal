5c0ac54ecf61e26b564c5c4f58112bcafd374083
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-07-20 09:23:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 API_ID = int(os.environ["API_ID"])
 API_HASH = os.environ["API_HASH"]
-STRING = os.environ["STRING_SESSION"]
+TOKEN = os.environ["BOT_TOKEN"]
 USERNAME = os.environ["USERNAME"]
 P = os.environ["PASSWORD"]
 LOG_ID = os.environ["LOG_ID"]
